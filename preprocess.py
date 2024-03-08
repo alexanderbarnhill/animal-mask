8912a4f6707f5109e7ed64a30f2e14be0afdf48d
Alex <alex@alexanderbarnhill.com>
2024-03-08 13:00:50 +0100

    Reformat slightly

--- preprocess.py
+++ preprocess.py
@@ -6,27 +6,43 @@ from data.model import SpeciesConfiguration, OrcaConfiguration
 from utilities.viewing import plot_spectrogram
 
 
-def get_transforms(configuration: SpeciesConfiguration):
-
+def get_transforms(configuration: SpeciesConfiguration) -> T.Compose:
     transforms = [
-        lambda fn: T.load_audio_file(fn, sr=configuration.sample_rate),
-        T.PreEmphasize(configuration.pre_emphasis),
-        T.Spectrogram(configuration.n_fft, configuration.hop_length, center=False),
-        T.Amp2Db(min_level_db=configuration.min_level_db, stype="power"),
-
+        lambda fn: T.load_audio_file(
+            file_name=fn,
+            sr=configuration.sample_rate),
+        T.PreEmphasize(
+            factor=configuration.pre_emphasis),
+        T.Spectrogram(
+            n_fft=configuration.n_fft,
+            hop_length=configuration.hop_length,
+            center=False),
+        T.Amp2Db(
+            min_level_db=configuration.min_level_db,
+            stype="power")
     ]
 
     if configuration.normalization_method == "min_max":
-        transforms.append(T.MinMaxNormalize())
+        transforms.append(
+            T.MinMaxNormalize()
+        )
     else:
-        transforms.append(T.Normalize(min_level_db=configuration.min_level_db, ref_level_db=configuration.ref_level_db))
+        transforms.append(
+            T.Normalize(min_level_db=configuration.min_level_db,
+                        ref_level_db=configuration.ref_level_db)
+        )
 
-    transforms.append(T.PaddedSubsequenceSampler(sequence_length=128, random=False, dim=1))
+    transforms.append(
+        T.PaddedSubsequenceSampler(
+            sequence_length=128,
+            random=False,
+            dim=1)
+    )
     return T.Compose(transforms)
 
 
 def load_create_spectrogram(audio_file: str, configuration: SpeciesConfiguration) -> Tensor:
-    t = get_transforms(configuration)
+    t = get_transforms(configuration=configuration)
     return t(audio_file)
 
 
